a92ed8e26e794c330418c9266e1b70c3c6722ad8
ayrton <ayrton.parkinson1@gmail.com>
2023-08-15 08:02:07 -0400

    Add #backtrack method

--- lib/sudoku_solver.rb
+++ lib/sudoku_solver.rb
@@ -10,4 +10,55 @@ class SudokuSolver
     @solution = board
     @solved = false
   end
+
+  # Solve board using backtracking - Returns 2d array of solution
+  def backtrack
+    return solution if solved
+
+    dfs(0)
+    solution
+  end
+
+  private
+
+  def dfs(index)
+    return true if index >= 80
+
+    row = index / 9
+    col =  index % 9
+    return dfs(index + 1) unless solution[row][col].zero?
+
+    # Attempt to solve using 1-9, return true if solution is found
+    (1..9).each do |v|
+      next unless valid_guess?(row, col, v)
+
+      solution[row][col] = v
+      return true if dfs(index + 1)
+
+      solution[row][col] = 0
+    end
+
+    false
+  end
+
+  def valid_guess?(row, col, value)
+    # Check row
+    return false if solution[row].include?(value)
+
+    # Check col
+    9.times do |r|
+      return false if solution[r][col] == value
+    end
+
+    # Check box
+    row_start = 3 * (row / 3)
+    col_start = 3 * (col / 3)
+    (row_start...row_start + 3).each do |r|
+      (col_start...col_start + 3).each do |c|
+        return false if solution[r][c] == value
+      end
+    end
+
+    true
+  end
 end
