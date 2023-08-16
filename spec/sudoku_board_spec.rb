a4131127e6ef5bf36d4f86a2bba51de3408447ac
ayrton <ayrton.parkinson1@gmail.com>
2023-08-16 08:15:16 -0400

    Add test for #empty_cell?

--- spec/sudoku_board_spec.rb
+++ spec/sudoku_board_spec.rb
@@ -115,7 +115,7 @@ describe SudokuBoard do
     end
   end
 
-  describe 'valid_guess?' do
+  describe '#valid_guess?' do
     it 'returns true if guess is valid' do
       board = [
         [5, 3, 0, 0, 7, 0, 0, 0, 0],
@@ -148,6 +148,42 @@ describe SudokuBoard do
 
       sudoku = SudokuBoard.new(board)
       expect(sudoku.valid_guess?(0, 2, 5)).to eq(false)
+    end
+  end
+
+  describe '#empty_cell?' do
+    it 'returns true if the cell is empty (zero value)' do
+      board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+      ]
+
+      sudoku = SudokuBoard.new(board)
+      expect(sudoku.empty_cell?(0, 2)).to eq(true)
+    end
+
+    it 'returns false if the cell is not empty (non-zero value)' do
+      board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+      ]
+
+      sudoku = SudokuBoard.new(board)
+      expect(sudoku.empty_cell?(0, 0)).to eq(false)
     end
   end
 end
