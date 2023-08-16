bf566949f9eb22bea58c1c875d62010fc648ecb8
ayrton <ayrton.parkinson1@gmail.com>
2023-08-16 07:20:23 -0400

    Refactor #backtracking to improve speed

--- lib/sudoku_solver.rb
+++ lib/sudoku_solver.rb
@@ -3,61 +3,75 @@
 # Sudoku Solving Class
 # Initialize with 2d array representation of sudoku board
 class SudokuSolver
-  attr_reader :board, :solution, :solved
+  attr_reader :board, :solution
 
   def initialize(board)
     @board = board
     @solution = board
-    @solved = false
+    @solution_properties = create_board_properties_hash
   end
 
-  # Solve board using backtracking - Returns 2d array of solution
+  # Attenpt to solve board using backtracking/dfs and return 2d array of solution
   def backtrack
-    return solution if solved
-
     dfs(0)
     solution
   end
 
   private
 
+  # Create and return hash containing properties of board for easy validation (row, col, and box values)
+  def create_board_properties_hash
+    board_properties = Hash.new { |h, k| h[k] = Array.new(9) { Array.new(10) } }
+
+    board.each_index do |r|
+      board[r].each_with_index do |value, c|
+        next if value.zero?
+
+        b = 3 * (r / 3) + (c / 3)
+        board_properties[:row][r][value] = true
+        board_properties[:col][c][value] = true
+        board_properties[:box][b][value] = true
+      end
+    end
+
+    board_properties
+  end
+
   def dfs(index)
     return true if index >= 81
 
-    row = index / 9
-    col =  index % 9
+    row, col, box = index / 9
+    col = index % 9
+    box = 3 * (row / 3) + (col / 3)
+
+    # Move to next square if already filled in
     return dfs(index + 1) unless solution[row][col].zero?
 
     # Attempt to solve using 1-9, return true if solution is found
-    (1..9).each do |v|
-      next unless valid_guess?(row, col, v)
+    (1..9).each do |value|
+      next unless valid_guess?(row, col, box, value)
 
-      solution[row][col] = v
-      return true if dfs(index + 1)
+      @solution_properties[:row][row][value] = true
+      @solution_properties[:col][col][value] = true
+      @solution_properties[:box][box][value] = true
 
-      solution[row][col] = 0
+      if dfs(index + 1)
+        solution[row][col] = value
+        return true
+      end
+
+      @solution_properties[:row][row][value] = false
+      @solution_properties[:col][col][value] = false
+      @solution_properties[:box][box][value] = false
     end
 
     false
   end
 
-  def valid_guess?(row, col, value)
-    # Check row
-    return false if solution[row].include?(value)
-
-    # Check col
-    9.times do |r|
-      return false if solution[r][col] == value
-    end
-
-    # Check box
-    row_start = 3 * (row / 3)
-    col_start = 3 * (col / 3)
-    (row_start...row_start + 3).each do |r|
-      (col_start...col_start + 3).each do |c|
-        return false if solution[r][c] == value
-      end
-    end
+  def valid_guess?(row, col, box, value)
+    return false if @solution_properties[:row][row][value] ||
+                    @solution_properties[:col][col][value] ||
+                    @solution_properties[:box][box][value]
 
     true
   end
