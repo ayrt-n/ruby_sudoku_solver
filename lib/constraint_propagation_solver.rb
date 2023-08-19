f1c5ab85b91dfa51c9a64de88ac7e5364ebb6bc5
ayrton <ayrton.parkinson1@gmail.com>
2023-08-19 12:18:13 -0400

    Complete implementation of constraint propagation algorithm via backtracking

--- lib/constraint_propagation_solver.rb
+++ lib/constraint_propagation_solver.rb
@@ -24,10 +24,32 @@ class ConstraintPropagationSolver
       continue ||= propagate_sets(constraints)
     end
 
-    [true, sudoku.board]
+    [dfs(sudoku, constraints, 0), sudoku.board]
   end
 
   private
+
+  def dfs(sudoku, constraints, index)
+    return true if index >= 81
+
+    row = index / 9
+    col = index % 9
+
+    # Move to next square if already filled in
+    return dfs(sudoku, constraints, index + 1) unless sudoku.empty_cell?(row, col)
+
+    # Attempt to solve using remaining constrained values, return true if solution is found
+    constraints[row][col].each do |value|
+      next unless sudoku.valid_guess?(row, col, value)
+
+      sudoku.guess(row, col, value)
+      return true if dfs(sudoku, constraints, index + 1)
+
+      sudoku.remove_guess(row, col)
+    end
+
+    false
+  end
 
   def propagate_constraint(constraints, row, col, constraint)
     # Propagate constraint across row
